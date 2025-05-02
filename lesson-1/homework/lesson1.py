cea9d4494687af07cd231c31c3039e813092e390
Nozimakhonim Khaydarova <nozima0770@gmail.com>
2025-05-02 12:05:12 +0500

    Create lesson1.py

---
+++ lesson-1/homework/lesson1.py
@@ -0,0 +1,30 @@
+# 1
+side= float(5)
+perimeter = 4 * side
+area = side * side
+
+print("Perimeter of the square:",perimeter)
+print("Area of the square", area)
+
+# 2
+import math
+diameter = float(6)
+circumference = math.pi * diameter
+print("Circumference (length) of the circle:", circumference)
+# 3
+a = float(5)
+b = float(6)
+mean = (a+b)/2
+print("Mean of two numbers:", mean)
+
+# 4
+a= float(5)
+b = float(6)
+sum_result = a+b
+product_result = a*b
+square_a = a**2
+square_b = b**2
+print("Sum:", sum_result)
+print("Product:", product_result)
+print("Square of a", square_a)
+print("Square of b", square_b)
